d0b65a825980d2eeef37476f485f06bdeff09b45
hismichael <hismichael@qq.com>
2016-07-18 23:33:54 +0800

    wrappers of request/response

---
+++ Core/WebRequest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace Greatbone.Core
+{
+  public struct WebRequest : IDataInput
+  {
+    readonly HttpRequest _impl;
+
+    private IFormCollection _form;
+
+    private JsonBin _json;
+
+    internal WebRequest(HttpRequest impl)
+    {
+      _impl = impl;
+
+      _form = null;
+      _json = null;
+      string ctype = impl.ContentType;
+      if ("application/x-www-form-urlencoded".Equals(ctype))
+      {
+        _form = impl.Form;
+      }
+      else if ("application/json".Equals(ctype))
+      {
+        _json = new JsonBin(12);
+      }
+    }
+
+    public bool GotStart()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool GotEnd()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool Got(string name, out int value)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool Got(string name, out decimal value)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool Got(string name, out string value)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool Got<T>(string name, out List<T> value) where T : IData
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
---
+++ Core/WebResponse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace Greatbone.Core
+{
+  public struct WebResponse : IDataOutput
+  {
+    HttpResponse _impl;
+
+
+    public void PutStart()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void PutEnd()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Put(string name, int value)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Put(string name, decimal value)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Put(string name, string value)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Put<T>(string name, List<T> value) where T : IData
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
