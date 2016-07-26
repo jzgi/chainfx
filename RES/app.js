2e27c0090f2c877e78ae5678eb33ac2c986c22fd
hismichael <hismichael@qq.com>
2016-07-26 15:27:01 +0800

    ui ajax

--- RES/app.js
+++ RES/app.js
@@ -1,15 +1,71 @@
-$(document).foundation();
 
-$(".off-canvas-submenu").hide();
 
-$(".off-canvas-submenu-call")
-    .click(function() {
 
-        var icon = $(this).parent().next(".off-canvas-submenu").is(':visible') ? '+' : '-';
+$(document)
+    .ready(function() {
+            // insert hot celebs
+            $.ajax({
+                type: "GET",
+                url: "hotcelebs.json",
+                data: "",
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                error: function(x, e) { alert(e.responseText); },
+                success: function(lst) {
+                    $.each(lst,
+                        function(index, value) {
+                            $('#panel1')
+                                .append('<input type="checkbox" id="checkbox_' +
+                                    value.id +
+                                    '" checked="checked" value="0" />&nbsp;<div style = "display:inline;" > ' +
+                                    value.name +
+                                    '</div >  < br /  > ');
 
-        $(this).parent().next(".off-canvas-submenu").slideToggle('fast');
+                        });
+                }
+            });
 
-        $(this).find("span").text(icon);
+            // insert hot brands
+            $.ajax({
+                type: "GET",
+                url: "hotbrands.json",
+                data: "",
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                error: function(x, e) { alert(e.responseText); },
+                success: function(lst) {
+                    $.each(lst,
+                        function(index, value) {
+                            $('#panel2')
+                                .append('<input type="checkbox" id="checkbox_' +
+                                    value.id +
+                                    '" checked="checked" value="0" />&nbsp;<div style = "display:inline;" > ' +
+                                    value.name +
+                                    '</div >  < br /  > ');
 
-    });
+                        });
+                }
+            });
 
+            $.ajax({
+                type: "GET",
+                url: "hotworks.json",
+                data: "",
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                error: function(x, e) { alert(e.responseText); },
+                success: function(lst) {
+                    $.each(lst,
+                        function(index, value) {
+                            $('#panel3')
+                                .append('<input type="checkbox" id="checkbox_' +
+                                    value.id +
+                                    '" checked="checked" value="0" />&nbsp;<div style = "display:inline;" > ' +
+                                    value.name +
+                                    '</div >  < br /  > ');
+
+                        });
+                }
+            });
+        }
+    )
